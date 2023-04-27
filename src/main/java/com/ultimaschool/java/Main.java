37f8545beab66073b20c17b311902cd7991f9c20
rafaaxits <rafaaxits@gmail.com>
2023-04-27 15:25:18 -0300

    adding new attributes for the Client class

--- src/main/java/com/ultimaschool/java/Main.java
+++ src/main/java/com/ultimaschool/java/Main.java
@@ -4,12 +4,15 @@ import com.ultimaschool.java.clientes.Cliente;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente clienteM = new Cliente("123.456.789-00", "joao_teste@mail.com",
-                "João Teste da Silva", 'M');
-        Cliente clienteF = new Cliente("987.654.321-00", "maria_teste@mail.com",
-                "Maria Teste da Silva", 'F');
-        Cliente clienteVazio = new Cliente("000.000.000-00", "vazio_teste@mail.com",
-                "Vazio Teste da Silva", 'V');
+        Cliente clienteM = new Cliente("João", "Teste da", "Silva",
+                "123.456.789-00", "10/01/1993", 'F', "joao_teste@mail.com",
+                "Av Agamenom", "(81) 9.9999-9999");
+        Cliente clienteF = new Cliente("Maria", "Teste da", "Silva",
+                "987.654.321-00", "01/10/2003", 'F', "maria_teste@mail.com",
+                "Av Boa vista", "(81) 9.1111-1111");
+        Cliente clienteVazio = new Cliente("Vazio", "Teste da", "Silva",
+                "000.000.000-00", "01/01/2001", 'V', "vazio_teste@gmail.com",
+                "Av vazia", "(00) 9.0000-0000");
         System.out.println(clienteM.toString());
         System.out.println(clienteF.toString());
         System.out.println(clienteVazio.toString());
--- src/main/java/com/ultimaschool/java/clientes/Cliente.java
+++ src/main/java/com/ultimaschool/java/clientes/Cliente.java
@@ -1,32 +1,40 @@
 package com.ultimaschool.java.clientes;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class Cliente {
-    private String cpf;
-    private String email;
     private String nomeCompleto;
+    private String primeiroNome;
+
+    private String nomesDoMeio;
+
+    private String sobrenome;
+    private String cpf;
+
+    private String dataDeNascimento; // dd/MM/YYYY ou MM/dd/YYYY
+
+    private int idadeAtual;
     private char genero;
+    private String email;
+    private String endereco;
+    private String telefone;
 
-    public Cliente(String cpf, String email, String nomeCompleto, char genero) {
+    public Cliente(String primeiroNome, String nomesDoMeio, String sobrenome, String cpf,
+                   String dataDeNascimento, char genero, String email, String endereco,
+                   String telefone) {
+        this.primeiroNome = primeiroNome;
+        this.nomesDoMeio = nomesDoMeio;
+        this.sobrenome = sobrenome;
+        this.nomeCompleto = primeiroNome + " " + nomesDoMeio + " " + sobrenome;
         this.cpf = cpf;
-        this.email = email;
-        this.nomeCompleto = nomeCompleto;
+        this.dataDeNascimento = dataDeNascimento;
+        this.idadeAtual = definirIdadeAtual();
         this.genero = genero;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
     }
 
     public String getNomeCompleto() {
@@ -37,12 +45,115 @@ public class Cliente {
         this.nomeCompleto = nomeCompleto;
     }
 
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getNomesDoMeio() {
+        return nomesDoMeio;
+    }
+
+    public void setNomesDoMeio(String nomesDoMeio) {
+        this.nomesDoMeio = nomesDoMeio;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public int getIdadeAtual() {
+        return idadeAtual;
+    }
+
+    public void setIdadeAtual(int idadeAtual) {
+        this.idadeAtual = idadeAtual;
+    }
+
     public char getGenero() {
         return genero;
     }
 
     public void setGenero(char genero) {
         this.genero = genero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    private int definirIdadeAtual() {
+        return recuperaAnoAtual() - recuperarAnoNascimento();
+    }
+
+    private int recuperarAnoNascimento() {
+        Calendar calendario = Calendar.getInstance();
+        Date dataDoNascimentoCliente;
+        try {
+            dataDoNascimentoCliente = definirFormatoData("dd/MM/yyyy").parse(this.dataDeNascimento);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        calendario.setTime(dataDoNascimentoCliente);
+        return calendario.get(calendario.YEAR);
+    }
+
+    private int recuperaAnoAtual() {
+        Date diaAtual = new Date();
+        Calendar calendario = Calendar.getInstance();
+        calendario.setTime(diaAtual);
+        return calendario.get(calendario.YEAR); //2023
+    }
+
+    private SimpleDateFormat definirFormatoData(String formatoData){
+        if(formatoData == "") {
+            return new SimpleDateFormat("dd/MM/yyyy");
+        } else {
+            return new SimpleDateFormat(formatoData);
+        }
     }
 
     private String tratamentoGenereo() {
@@ -58,6 +169,8 @@ public class Cliente {
     @Override
     public String toString() {
         return tratamentoGenereo() + " " + getNomeCompleto() + ", com CPF " +
-                getCpf() + " e e-mail " + getEmail();
+                getCpf() + ", data de nascimento " + getDataDeNascimento() +
+                " com idade de " + getIdadeAtual() + ", e-mail " + getEmail() +
+                ", endereço " + getEndereco() + " e telefone " + getTelefone();
     }
 }
